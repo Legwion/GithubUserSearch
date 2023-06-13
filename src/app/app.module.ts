3c392f7c97e1cdc667999ba1d4783c130297e8d5
Legwion <mrjoanser@gmail.com>
2023-06-13 12:47:55 +0200

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.module.ts 	modified:   src/index.html 	src/app/components/ 	src/custom-theme.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+//Components
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component'; 
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
+
+
+//  Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    UserComponent,
+    UserTableComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
