79fd21390e1284a751a74486ad51427cb67fc6df
Legwion <mrjoanser@gmail.com>
2023-06-15 13:34:42 +0200

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/styles.sass 	modified:   src/app/app.module.ts 	modified:   src/styles.sass 	src/app/components/ 	src/app/services/ 	src/custom-theme.scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: 'table', component: UserTableComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirección de ruta vacía a '/home'
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
